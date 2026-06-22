5330376a86547497e299f2457797e583ff41ef1f
forcebeyondweb <oscar.wang@forcebeyond.com>
2026-06-22 10:04:35 -0400

    Update projectId in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,7 +8,7 @@ const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
 export default defineConfig({
 	name: 'forcebeyond',
 	title: 'ForceBeyond Content',
-	projectId: env.PUBLIC_SANITY_PROJECT_ID || '00000000',
+	projectId: env.PUBLIC_SANITY_PROJECT_ID || 'hxb9nbp2',
 	dataset: env.PUBLIC_SANITY_DATASET || 'production',
 	plugins: [structureTool()],
 	schema: { types: schemaTypes }
