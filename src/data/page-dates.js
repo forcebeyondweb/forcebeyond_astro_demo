0f9c4071c62dc05a7b0e19df1408d7c617cc9eb5
Oscar Wang <oscar.wang@forcebeyond.com>
2026-08-12 17:15:52 -0400

    Add white paper and fix cross page mismatch

---
+++ src/data/page-dates.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef {Object} PageDateEntry
+ * @property {string=} published
+ * @property {string=} modified
+ */
+
+/** @type {Record<string, PageDateEntry>} */
+export const pageDates = {
+    // Add dates only when you are ready.
+
+    "/titanium-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-11T15:31:00-04:00",
+    },
+};
