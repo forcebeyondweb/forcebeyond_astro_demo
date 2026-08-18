83fbea17b4679b6da15f2fe0292b66e7b068d449
Oscar Wang <oscar.wang@forcebeyond.com>
2026-08-18 10:30:52 -0400

    Add superalloy-solutions page

--- src/data/page-dates.js
+++ src/data/page-dates.js
@@ -6,10 +6,392 @@
 
 /** @type {Record<string, PageDateEntry>} */
 export const pageDates = {
-    // Add dates only when you are ready.
-
+    "/404-page/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/a356-t6-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/about-forcebeyond/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/aerospace-titanium-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/agricultural/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/aluminum-copper-alloys/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/aluminum-die-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/assembly-and-kitting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/automotive/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/c69300-brass-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/carbon-steel-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/careers/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/closed-die-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/cold-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/contact-us/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/data-center-power/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/die-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/duplex-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/duplex-stainless-steel-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/energy-power-generation/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/global-footprint-logistics/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/heat-treatment/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/high-performance-superalloy-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/high-performance-superalloy-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/high-performance-superalloy-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/high-precision-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/high-strength-beta-titanium-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/hot-isostatic-pressing/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/hvac-flow/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-625-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-625-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-625-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-718-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-718-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-718-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-718-vs-625-forging-guide/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/inconel-vacuum-precision-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/industrial-titanium-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/industrial-titanium-sand-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/investment-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/machined-titanium-parts/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/magnesium-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/manufacturing-design-guides/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/manufacturing-process-animations/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/material-cross-reference-charts/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/materials-technical-specs/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/medical/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/mining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/multi-axis-cnc/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/near-net-shape-titanium-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/near-net-shape-titanium-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/news-and-case-studies/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/nuclear-smr/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/oil-gas/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/open-die-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/precision-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/quality-assurance-certifications/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/quality-global-supply-chain/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/request-for-quote/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/rolled-ring-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/sand-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/secondary-operations/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/some-of-our-customers/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/space-defense/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/stainless-carbon-steel/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/stainless-steel-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/stainless-steel-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/superalloy-casting-guide/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/superalloys-exotic-metals/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/surface-finishing/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/testing-inspection-capabilities/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-aerospace-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-aerospace-forging/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-alloys/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-boat-propeller/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-bolts/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-compressor-impeller/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-exhaust-manifold/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-fan-blades/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
     "/titanium-forging/": {
         published: "2026-03-05T11:07:47-05:00",
-        modified: "2026-08-11T15:31:00-04:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-impeller/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-investment-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-jet-engine-blades/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-machining/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-pump-body-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-pump-impeller/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-solutions/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-stator-titanium-brake-rotor/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-turbine-blade/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-turbine-impeller/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/titanium-valve-body-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/zinc-die-casting/": {
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
+    },
+    "/superalloy-solutions/":{
+        published: "2026-03-05T11:07:47-05:00",
+        modified: "2026-08-14",
     },
 };
